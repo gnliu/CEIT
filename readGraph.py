6bf70bd361c6cf84e1e33fc2f245b03c10a395de
gnliu <guannliu@gmail.com>
2013-10-30 23:52:32 -0400

    Update readGraph.py

--- readGraph.py
+++ readGraph.py
@@ -20,8 +20,7 @@ class Graph:
 			fe = int(edge[0])
 			te = int(edge[1])
 			self.G.add_edge(fe , te)
-		#print x,y
-		#return ReadGraph.G
+		
 class weightedGraph:
 	def __init__(self, fname, sep):
 		self.G = nx.Graph()
@@ -39,22 +38,12 @@ class weightedGraph:
 #			if u == v:
 #				continue
 			w = int(str[2])
-#			print u,v,w
 			if self.G.has_edge(u, v):
 				self.G[u][v]['weight'] += w
 			else:
 				self.G.add_edge(u, v, weight=w)
-		print rowid
 		return self.G
 
-if __name__ == "__main__":
-	
-	G = ReadGraph("data/BiEuscc.txt","\t", False).G
-#	G = G.to_undirected(True)
-	print nx.average_clustering(G)
-	print G.number_of_nodes(), G.number_of_edges(), np.mean([G.degree(n) for n in G.nodes()])
-	
-	print sorted(G.degree().items(), key=lambda d:d[1], reverse=True)[0:10]
 			
 			
 		
